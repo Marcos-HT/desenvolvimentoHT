f1497bbd7b4d41da6af70748c7f57f2471ec9b33
Marcos-HT <marcos@hoteltechbr.com>
2025-04-02 16:33:49 -0300

    att

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // Seleciona os botões dentro da SEGUNDA seção de botões, que contém os botões da configuração de luz.
+    // Seleciona os botões dentro da PRIMEIRA seção de botões, que contém os botões da configuração de luz.
     const botoesLuz = document.querySelectorAll('.apresentacao__links__botoes-secao:nth-child(1) .apresentacao__links__navegacao');
 
     botoesLuz.forEach(botao => {
@@ -14,11 +14,11 @@ document.addEventListener('DOMContentLoaded', function() {
             const endpoint = endpointMapLuz[this.textContent.trim()]; // Obtém o endpoint correspondente ao texto do botão
 
             if (endpoint) {
-                let url = 'https://192.168.10.108:81/' + endpoint; // URL base para os botões de luz
+                let url = 'http://10.31.0.14:3000/' + endpoint; // URL base para os botões de luz
 
-                // Se o botão for "Update", use um IP/URL diferente (SE NECESSÁRIO)
+                // Se o botão for "Update", usa um IP/URL diferente (SE NECESSÁRIO)
                 if (endpoint === 'update') {
-                    url = 'https://192.168.10.108:8080/update'; // Substitua pelo IP/Porta correto para o botão Update (se diferente)
+                    url = 'https://10.31.0.14:3000/update'; // Substitua pelo IP/Porta correto para o botão Update (se diferente)
                 }
 
                 // Faz a requisição HTTP (fetch)
@@ -46,4 +46,48 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         });
     });
+    
+    // Seleciona os botões dentro da SEGUNDA seção de botões, que contém os botões da configuração de ar.
+     const botoesAr = document.querySelectorAll('.apresentacao__links__botoes-secao:nth-child(2) .apresentacao__links__navegacao');
+
+     botoesAr.forEach(botao => {
+        botao.addEventListener('click', function() {
+            //Mapeia os nomes dos botões para os endpoints corretos
+            const endpointMapAr = {
+                'On': 'ar_on',
+                'Off': 'ar_off',
+                '+ Ar': 'ar_mais',
+                '- Ar': 'ar_menos'
+            };
+            const endpoint = endpointMapAr[this.textContent.trim()]; // Obtém o endpoint correspondente ao texto do botão
+
+            if(endpoint){
+                let url = 'https://10.31.0.14:3000/' + endpoint; // URL base para os botões de ar
+
+                // Faz a requisição HTTP (fetch)
+                fetch(url)
+                .then(response => {
+                    if(!response.ok){
+                        throw new Error('Erro na requisição: '+ response.status);
+                    }
+                    return response.text(); // Obtém o corpo da resposta como texto
+                })
+                .then(data =>{
+                    //aplica efeito visual ao botão
+                    this.classList.add('clicked');
+                    setTimeout(() => {
+                        this.classList.remove('clicked');
+                    },300);
+                    console.log(data);
+                })
+                .catch(error => {
+                    console.error('Erro:', error);
+                });
+            } else {
+                console.error("Endpoint não encontrado para o botão:", this.textContent.trim());
+            }
+
+        });
+     });
+
 });
